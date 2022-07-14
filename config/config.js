f65af4d239869ffd185e3777aa08cd7784bb1c4b
Jfilippo06 <jfilippo04@gmail.com>
2022-07-14 06:09:22 +0200

    news

--- config/config.js
+++ config/config.js
@@ -1,26 +1,12 @@
 require('dotenv').config()
 
 module.exports = {
-    "development": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database":  process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "port": process.env.DB_PORT,
-        "dialect": "mysql"
-    },
-    "test": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "dialect": "mysql"
-    },
-    "production": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "dialect": "mysql"
-    }
+    dialect: process.env.DB_DIALECT,
+    storage: process.env.DB_URI,
+
+    seederStorage: 'sequelize',
+    seederStorageTableName: 'seeds',
+
+    migrationStorage: 'sequelize',
+    migrationStorageTableName: 'migrations'
 }
---
+++ migrations/20220714020924-create-news.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('News', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        references: {model: 'category', key: 'id'}
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('News');
+  }
+};
