82dbf80bd63a007a4d57f06176acf8ac539584ff
Jfilippo06 <jfilippo04@gmail.com>
2022-07-15 00:29:42 +0200

    config.js

--- config/config.js
+++ config/config.js
@@ -7,20 +7,23 @@ module.exports = {
         "database":  process.env.DB_NAME,
         "host": process.env.DB_HOST,
         "port": process.env.DB_PORT,
-        "dialect": "mysql"
+        "dialect": process.env.DB_DIALECT,
+        "storage": process.env.DB_STORAGE,
     },
     "test": {
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_NAME,
         "host": process.env.DB_HOST,
-        "dialect": "mysql"
+        "dialect": process.env.DB_DIALECT,
+        "storage": process.env.DB_STORAGE,
     },
     "production": {
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_NAME,
         "host": process.env.DB_HOST,
-        "dialect": "mysql"
+        "dialect": process.env.DB_DIALECT,
+        "storage": process.env.DB_STORAGE,
     }
 }
