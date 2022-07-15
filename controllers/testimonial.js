e56644fe9d14894b8e1e0cacc99972d5a4a1fc31
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-07-15 20:14:03 -0300

    adding controllers/testimonial.js

---
+++ controllers/testimonial.js
@@ -0,0 +1,9 @@
+export async function createTestimonial(request, response) {}
+
+export async function getAllTestimonials(request, response) {}
+
+export async function getByIdTestimonial(request, response) {}
+
+export async function deleteByIdTestimonial(request, response) {}
+
+export async function putByIdTestimonial(request, response) {}
