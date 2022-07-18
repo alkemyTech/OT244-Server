4266f8427e35b5fe495723fcecfb06c11c0f2e7f
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-07-18 17:06:53 -0300

    adding modifications

--- controllers/testimonial.js
+++ controllers/testimonial.js
@@ -1,9 +1,17 @@
-export async function createTestimonial(request, response) {}
+async function createTestimonial(request, response) {}
 
-export async function getAllTestimonials(request, response) {}
+async function getAllTestimonials(request, response) {}
 
-export async function getByIdTestimonial(request, response) {}
+async function getByIdTestimonial(request, response) {}
 
-export async function deleteByIdTestimonial(request, response) {}
+async function deleteByIdTestimonial(request, response) {}
 
-export async function putByIdTestimonial(request, response) {}
+async function putByIdTestimonial(request, response) {}
+
+module.exports = {
+  createTestimonial,
+  getAllTestimonials,
+  getByIdTestimonial,
+  deleteByIdTestimonial,
+  putByIdTestimonial,
+};
