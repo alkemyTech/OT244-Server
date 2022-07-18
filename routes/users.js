3492a4deb6e753e60193e28197af1220df8ba366
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-07-18 16:58:01 -0300

    adding endpoint /auth/register

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const { createUser } = require("../controllers/user");
+const {
+  validateFields,
+  bodyFields,
+} = require("../middlewares/validateCreateUser");
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
+
+router.post("/auth/register", bodyFields, validateFields, createUser);
 
 module.exports = router;
