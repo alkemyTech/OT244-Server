498ab6c1419105e45f9aeec163f3448b38c99aa9
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-07-20 19:00:23 -0300

    auth/register route fix

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { createUser } = require("../controllers/auth");
+const {
+  validateFields,
+  bodyFields,
+} = require("../middlewares/validateCreateUser");
+const router = express.Router();
+
+// POST full path '/auth/register'
+router.post("/register", bodyFields, validateFields, createUser);
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,10 @@
 const express = require("express");
-const { createUser } = require("../controllers/user");
-const {
-  validateFields,
-  bodyFields,
-} = require("../middlewares/validateCreateUser");
+
 const router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
-
-router.post("/auth/register", bodyFields, validateFields, createUser);
 
 module.exports = router;
