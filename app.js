b93536dc8493ea2e944aad0c275a08c96e088374
martin <martinc1998a@gmail.com>
2022-07-25 11:10:06 -0300

    reverted changes

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ require('dotenv').config();
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
-const testimonialsRouter = require('./routes/testimonials');
 
 const app = express();
 app.use(cors());
@@ -29,7 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
-app.use('/testimonials', testimonialsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
