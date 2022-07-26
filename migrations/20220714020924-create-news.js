e53c1da4bc247cacc52ac3bc8d00a9c8adc67dfd
Jfilippo06 <jfilippo04@gmail.com>
2022-07-26 22:49:38 +0200

    fix migrations

--- migrations/20220714020924-create-news.js
+++ migrations/20220714020924-create-news.js
@@ -23,7 +23,7 @@ module.exports = {
       categoryId: {
         type: Sequelize.INTEGER,
         references: {
-          model: 'Categorys',
+          model: 'Categories',
           key: 'id',
         },
         onUpdate: 'CASCADE',
