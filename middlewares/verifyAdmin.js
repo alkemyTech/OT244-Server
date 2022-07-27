3afbf809df07859e5ee93e682c0fe1f02794882a
bluemountains88 <m.dariouk@gmail.com>
2022-07-26 22:25:54 -0300

    ADMIN_ROLE data added to environment constants

--- middlewares/verifyAdmin.js
+++ middlewares/verifyAdmin.js
@@ -1,5 +1,5 @@
 const verifyAdmin = (req, res, next) => {
-    if (req.user.roleId !== 1) {
+    if (req.user.roleId !== process.env.ADMIN_ROLE_ID) {
         return res.status(401).json({ message: "You are not authorized to perform this action!" });
     }
     next();
