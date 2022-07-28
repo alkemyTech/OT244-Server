57466021c721471c76fc994941cce111ade6ac75
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-07-28 17:42:37 -0300

    fixed send user data

--- controllers/user.js
+++ controllers/user.js
@@ -3,9 +3,9 @@
 
 const getDataUser= (req, res) => {
   const user= req.user
-    if(user){
-       return res.status(200).json(user)
-    
+  if (user) {
+    return res.status(200).json(user)
+  }
     else{
         return res.status(500).json({ msg: "An unexpected error occurred" });
    }
