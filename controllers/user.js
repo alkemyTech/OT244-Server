bf26366b872a405f5c593ac6fa6735277624cc88
Gonzalo Damián Fleitas <88991463+JAFleitas@users.noreply.github.com>
2022-07-27 23:45:33 -0300

    Fix

--- controllers/user.js
+++ controllers/user.js
@@ -1,22 +1,16 @@
-const { User } = require("../models")
 
 
-const getUserById = async (req, res) => {
-  const {id}= req.user
-  try {
-    const user = await User.findByPk(id, {
-      attributes: {
-        exclude: ["password","roleId"],
-      },
-    });
 
-    return res.status(200).json(user)
-  } catch (error) {
-    console.log(error)
-    return res.status(500).json({ msg: "An unexpected error occurred" });
-  }
+const getDataUser= (req, res) => {
+  const user= req.user
+    if(user){
+       return res.status(200).json(user)
+    
+    else{
+        return res.status(500).json({ msg: "An unexpected error occurred" });
+   }
 }
 
 module.exports = {
-  getUserById
+  getDataUser
 }
