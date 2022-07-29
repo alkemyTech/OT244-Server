b558206a79ac1f78b5bb65569e938bb691e57555
Jfilippo06 <jfilippo04@gmail.com>
2022-07-29 03:15:09 +0200

    routes middlewares express-validator

--- middlewares/validateCreateCategories.js
+++
@@ -1,8 +0,0 @@
-const { body } = require("express-validator");
-
-const bodyFieldsCreateCategory = body("name", "Enter a valid name!")
-  .trim()
-  .notEmpty()
-  .isString({ min:4})
-  .escape();
-module.exports = bodyFieldsCreateCategory;
