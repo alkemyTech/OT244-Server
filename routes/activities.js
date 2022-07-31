0b3750a412d4c3c46b486c7d44c0c6b9e74ac12f
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-07-31 10:37:43 -0300

    added path categories/:id

--- routes/activities.js
+++ routes/activities.js
@@ -1,9 +1,19 @@
 const express = require( "express" );
 const validationResult = require('../middlewares/validationResult')
-const { activities } = require('../middlewares/validationBody')
-const { addActivity } = require("../controllers/activities");
+const { activities, bodyUpdateActivityById } = require('../middlewares/validationBody')
+const { addActivity, updateActivityById } = require("../controllers/activities");
+const verifyAdmin=require("../middlewares/verifyAdmin")
 const router = express.Router();
 
 router.post("/", /* verifyAdmin, */ activities, validationResult, addActivity);
+
+// PUT activity by id
+router.put(
+  "/:id",
+  verifyAdmin,
+  bodyUpdateActivityById,
+  validationResult,
+  updateActivityById
+);
 
 module.exports = router;
