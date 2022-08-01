da16f646f4e04c271ca83996657226ce55db05ee
mar-romero <romero-ar@hotmail.com>
2022-08-01 20:28:25 -0300

    Update testimonials.js

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,9 +1,9 @@
 const express = require( "express" );
 const { testimonials } = require('../middlewares/validationBody');
-const { createTestimonial } = require('../controllers/testimonial');
+const { createTestimonial, deleteByIdTestimonial} = require('../controllers/testimonial');
 const validationResult = require('../middlewares/validationResult')
 const router = express.Router();
 
 router.post('/', /* verifyAdmin */ testimonials, validationResult, createTestimonial);
-
+router.post('/', /* verifyAdmin */ testimonials, validationResult, deleteByIdTestimonial);
 module.exports = router;
