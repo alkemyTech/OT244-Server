cc6d8773976f4b9d76943c230522839c082ab051
Jfilippo06 <jfilippo04@gmail.com>
2022-08-03 00:20:57 +0200

    route

--- routes/slides.js
+++ routes/slides.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const { postSlides } = require('../controllers/slides')
+const verifyAdmin = require('../middlewares/verifyAdmin')
 const router = express.Router()
 
-router.post('/', /* verifyAdmin, */ postSlides)
+router.post('/', verifyAdmin, postSlides)
 
 module.exports = router
