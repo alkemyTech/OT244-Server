d6c7aba7c7c21888ab6d94f2850b84614538eb4c
martin <martinc1998a@gmail.com>
2022-08-02 18:22:13 -0300

    implemented verify admin middleware to both routes

--- routes/news.js
+++ routes/news.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const validationResult = require('../middlewares/validationResult')
-const { news, bodyUpdateNews } = require("../middlewares/validationBody");
+const { news } = require("../middlewares/validationBody");
 const { createNews, updateNews } = require("../controllers/news");
 const router = express.Router();
 
 // POST for the admin to create news - full path "/news"
 
-router.post("/", /* verifyAdmin, */ news, validationResult, createNews);
+router.post("/", verifyAdmin, news, validationResult, createNews);
 
-router.put('/:id', /* verifyAdmin */  bodyUpdateNews, validationResult, updateNews);
+router.put('/:id', verifyAdmin, news, validationResult, updateNews);
 
 module.exports = router;
