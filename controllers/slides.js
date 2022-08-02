4de42c4f7d4cf57ae4c20faf809341fb02946a55
Jfilippo06 <jfilippo04@gmail.com>
2022-08-03 00:11:52 +0200

    fix

--- controllers/slides.js
+++ controllers/slides.js
@@ -1,20 +1,11 @@
-const s3 = require("../config/aws")
 const {Slides} = require("../models")
-require('dotenv').config()
 
 const postSlides = async (req,res) => {
     try {
-        const fileContent = Buffer.from(req.files.image.data, 'base64')
-        const params = {
-            Bucket: process.env.AWS_S3_BUCKET_NAME,
-            key: req.files.image.name,
-            Body: fileContent,
-        }
-        const uploadedImage = await s3.upload(params).promise()
         const newSlide = await Slides.create({
-            text: uploadedImage.Key,
-            imageUrl: uploadedImage.Location,
-            order: new Date(),
+            text: 'image-example',
+            imageUrl: 'https://image-example.com',
+            order: 'image-example-2022',
         })
         const data = { msg: "Slide created successfully", newSlide }
         return res.status(201).json(data)
