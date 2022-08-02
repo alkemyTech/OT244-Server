490fc48d44398bea4c4dec2b85a1333ead0b9721
mar-romero <romero-ar@hotmail.com>
2022-08-01 21:12:11 -0300

    Update news.js

--- routes/news.js
+++ routes/news.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const validationResult = require('../middlewares/validationResult')
 const { news } = require("../middlewares/validationBody");
-const { createNews,getAllNew} = require("../controllers/news");
+const { createNews,getNew} = require("../controllers/news");
 const router = express.Router();
 
 // POST for the admin to create news - full path "/news"
