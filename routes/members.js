9d94896d7365785af08d21f6ec6ea0cfe55c4a57
martin <martinc1998a@gmail.com>
2022-08-02 18:48:09 -0300

    added old validator to the new route

--- routes/members.js
+++ routes/members.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const validationResult = require('../middlewares/validationResult')
-const { members, bodyUpdateMember } = require("../middlewares/validationBody");
+const { members } = require("../middlewares/validationBody");
 const { createMember, updateMember } = require("../controllers/member");
 const router = express.Router()
 
 router.post("/", members, validationResult, createMember)
 
-router.put('/', bodyUpdateMember, validationResult, updateMember);
+router.put('/', members, validationResult, updateMember);
 
 module.exports = router
