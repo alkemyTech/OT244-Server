73133465157a047abf5dd63ee0566a06ade8cc4b
Sebastian Amaya <91269682+sebasam@users.noreply.github.com>
2022-08-03 15:17:25 -0500

    get all members (#33)
    * routes and cotroller members updated, it can get all members in endpoint /members
    
    * verifyAdmin added

--- routes/members.js
+++ routes/members.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const validationResult = require('../middlewares/validationResult')
 const { members } = require("../middlewares/validationBody");
-const { createMember, deleteMember } = require("../controllers/member");
+const verifyAdmin = require("./../middlewares/verifyAdmin")
+const { createMember, deleteMember, getMembers } = require("../controllers/member");
 const router = express.Router()
 
 router.post("/", members, validationResult, createMember)
+
 router.delete("/:id", deleteMember, )
+
+router.get("/", verifyAdmin, getMembers)
 
 module.exports = router
