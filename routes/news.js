7378440ebee48b7dcf4bf9e859a24e50f02d3971
sebasam <sebasamayaxd@hotmail.com>
2022-08-03 00:02:01 -0500

    verifyAdmin added

--- routes/news.js
+++ routes/news.js
@@ -2,12 +2,13 @@ const express = require("express");
 const validationResult = require('../middlewares/validationResult')
 const { news } = require("../middlewares/validationBody");
 const { createNews, deleteNew } = require("../controllers/news");
+const verifyAdmin = require("./../middlewares/verifyAdmin")
 const router = express.Router();
 
 // POST for the admin to create news - full path "/news"
 
 router.post("/", /* verifyAdmin, */ news, validationResult, createNews);
 
-router.delete("/:id", /*verifyAdmin*/ deleteNew);
+router.delete("/:id", verifyAdmin, deleteNew);
 
 module.exports = router;
