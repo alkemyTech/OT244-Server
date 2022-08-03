0dbf90195462eb0023ddb2e8be55e3df537f2257
Jfilippo06 <jfilippo04@gmail.com>
2022-08-03 22:32:33 +0200

    fix

--- controllers/slides.js
+++ controllers/slides.js
@@ -1,5 +1,13 @@
-
 const {Slides} = require("../models")
+
+const getSlides = async (req,res) => {
+    try {
+        const getData = await Slides.findAll({attributes: ['text','imageUrl','order']})
+        res.status(200).json(getData)
+      } catch (error) {
+        return res.status(500).json({ msg: "An unexpected error occurred" });
+      }
+}
 
 const postSlides = async (req,res) => {
     try {
@@ -15,21 +23,7 @@ const postSlides = async (req,res) => {
     }
 }
 
-
-
-
-
-
-const getSlides = async (req,res) => {
-    try {
-        const getData = await Slides.findAll({attributes: ['text','imageUrl','order']})
-        res.status(200).json(getData)
-      } catch (error) {
-        return res.status(500).json({ msg: "An unexpected error occurred" });
-      }
-}
-
 module.exports = {
     getSlides,
-
+    postSlides,
 }
--- routes/slides.js
+++ routes/slides.js
@@ -1,15 +1,9 @@
 const express = require('express')
-
-const { postSlides } = require('../controllers/slides')
+const { postSlides, getSlides } = require('../controllers/slides')
 const verifyAdmin = require('../middlewares/verifyAdmin')
 const router = express.Router()
 
 router.post('/', verifyAdmin, postSlides)
-
-
-
-
-router.get('/', /* verifyAdmin, */ getSlides)
-
+router.get('/', verifyAdmin, getSlides)
 
 module.exports = router
