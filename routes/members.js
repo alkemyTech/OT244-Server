d9072544c9a48580bbd4c1a7062dfbad71056ea9
sebasam <sebasamayaxd@hotmail.com>
2022-08-02 23:58:43 -0500

    verifyAdmin added

--- routes/members.js
+++ routes/members.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const validationResult = require('../middlewares/validationResult')
 const { members } = require("../middlewares/validationBody");
+const verifyAdmin = require("./../middlewares/verifyAdmin")
 const { createMember, getMembers } = require("../controllers/member");
 const router = express.Router()
 
 router.post("/", members, validationResult, createMember)
 
-router.get("/", getMembers)
+router.get("/", verifyAdmin, getMembers)
 
 module.exports = router
