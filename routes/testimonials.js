8aae999ec5efb66d29044c2e4eb75da0d2bd3736
mar-romero <65137011+mar-romero@users.noreply.github.com>
2022-08-05 21:50:16 -0300

    Delete testimonial (#42)
    * Delete testimonial
    
    * Update testimonials.js
    
    * Update testimonials.js
    
    * Cambios solicitados realizados
    
    Co-authored-by: Sebastian Amaya <91269682+sebasam@users.noreply.github.com>

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -3,9 +3,13 @@ const userAuthenticate = require('../middlewares/user-authenticate')
 const verifyAdmin = require("../middlewares/verifyAdmin")
 const validationResult = require('../middlewares/validationResult')
 const { testimonials } = require('../middlewares/validationBody');
-const { createTestimonial } = require('../controllers/testimonial');
+const { createTestimonial, deleteByIdTestimonial} = require('../controllers/testimonial');
+const validationResult = require('../middlewares/validationResult')
+
 const router = express.Router();
+const verifyAdmin  = require('../middlewares/verifyAdmin')
 
 router.post('/', userAuthenticate, verifyAdmin, testimonials, validationResult, createTestimonial);
+router.delete('/:id',  verifyAdmin, deleteByIdTestimonial);
 
 module.exports = router;
