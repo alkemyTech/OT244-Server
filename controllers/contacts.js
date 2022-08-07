d1980000c4192a5932aa38a406cd0de6e6935aeb
Jfilippo06 <jfilippo04@gmail.com>
2022-08-07 21:09:31 +0200

    Contacts

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,8 +1,9 @@
 const { listService } = require("../services/contacts")
 
 const listAll = async (req, res) => {
-    const listData = await listService(req)
-    res.status(201).json(listData)
+    const listData = await listService()
+    if (!listData) res.status(404).json('Emails not found')
+    else res.status(201).json(listData)
 }
 
 module.exports = {
