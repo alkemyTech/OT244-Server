b0324625faa46845c921ea6ea0a0d3bc2d31013c
mar-romero <romero-ar@hotmail.com>
2022-08-08 16:09:26 -0300

    Al testearlo me olvide de requerir lo que realice

--- routes/slides.js
+++ routes/slides.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const userAuthenticate = require('../middlewares/user-authenticate')
 const verifyAdmin = require("../middlewares/verifyAdmin")
-const { postSlides, getSlides } = require('../controllers/slides')
+const { postSlides, getSlides, deleteSlides} = require('../controllers/slides')
 const router = express.Router()
 
 router.post('/', userAuthenticate, verifyAdmin, postSlides)
