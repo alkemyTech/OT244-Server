33ef7e01e3ad21865e7487fd8c68e524ca4d42be
Martin Caceres <64352974+martincaceres2@users.noreply.github.com>
2022-08-08 21:05:22 -0300

    added endpoint to update the public organization data with its respective route and middlewares (#56)
    Co-authored-by: Sebastian Amaya <91269682+sebasam@users.noreply.github.com>

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,9 @@ const categoryRouter = require('./categories');
 const routerActivities = require('./activities');
 const commentsRouter = require('./comments');
 const slidesRouter = require('./slides')
+const organizationRouter = require('./organizations');
 const contactsRouter = require('./contacts');
+
 
 
 router.use('/', indexRouter);
@@ -24,8 +26,8 @@ router.use('/categories', categoryRouter);
 router.use('/activities', routerActivities);
 router.use('/comments', commentsRouter)
 router.use('/slides', slidesRouter);
+router.use('/organization', organizationRouter);
 router.use('/contacts', contactsRouter);
 
 
 module.exports = router;
-
