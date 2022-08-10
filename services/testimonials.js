506a27fd789adf55c1453da351c30395d9c2b219
Jfilippo06 <jfilippo04@gmail.com>
2022-08-10 03:12:07 +0200

    use dario's helpers

--- services/testimonials.js
+++ services/testimonials.js
@@ -1,11 +1,16 @@
 const { findAndCountAllTestimonials } = require("../DAL/testimonials")
+const { nextPage, prevPage } = require("../helpers/paginationTools")
 
-const testimonialsServiceGet = async (limit,offset,page) => {
+const testimonialsServiceGet = async (page,size) => {
+    const limit = size ? +size : 10
+    const offset = page ? page * limit : 0
     const data = await findAndCountAllTestimonials(limit,offset)
     const { count: totalItems, rows: testimonials } = data
     const currentPage = page ? +page: 0
     const totalPages = Math.ceil(totalItems / limit)
-    return { totalItems, testimonials, totalPages, currentPage }
+    const next = nextPage('testimonials', currentPage, totalPages)
+    const prev = prevPage('testimonials', currentPage, totalPages)
+    return { totalItems, testimonials, next, prev }
 }
 
 module.exports = {
