1c7be907d2160b32cb8477fc236250aeb0be1a24
Jfilippo06 <jfilippo04@gmail.com>
2022-08-12 20:17:00 +0200

    fix routes

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -3,7 +3,7 @@ const userAuthenticate = require('../middlewares/user-authenticate');
 const verifyAdmin = require("../middlewares/verifyAdmin");
 const validationResult = require('../middlewares/validationResult');
 const { testimonials } = require('../middlewares/validationBody');
-const { createTestimonial, deleteByIdTestimonial, getAllTestimonials} = require('../controllers/testimonial');
+const { createTestimonial, deleteByIdTestimonial, getAllTestimonials, putByIdTestimonial} = require('../controllers/testimonial');
 const router = express.Router();
 
 router.put('/:id', verifyAdmin , testimonials, putByIdTestimonial)
