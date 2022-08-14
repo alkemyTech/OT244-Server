128cc592d7872680ccbb9c3df656a0ebd2702fbe
Jfilippo06 <jfilippo04@gmail.com>
2022-08-14 23:59:34 +0200

    put

--- test/activities.test.js
+++ test/activities.test.js
@@ -79,3 +79,80 @@ describe('POST verified body', () =>{
         .expect('Content-Type', /application\/json/)
     })
 })
+
+describe('PUT JWT invalid', () =>{
+    test('There is no token', async() => {
+        const api = await supertest(app)
+        .put("/activities/1")
+        .expect(403)
+    })
+})
+
+describe('PUT Unauthorized', () =>{
+    test('User is not authorize', async() => {
+        const api = await supertest(app)
+        .put("/activities/1")
+        .set('Authorization', `Bearer ${standard}`)
+        .expect(401)
+    })
+})
+
+describe('PUT verified body', () =>{
+    test('There is no name', async() => {
+        const api = await supertest(app)
+        .put("/activities/1")
+        .send({content:"content", image:"image.example"})
+        .set('Authorization', `Bearer ${admin}`)
+        .expect(400)
+    })
+})
+
+describe('PUT verified body', () =>{
+    test('There is no content', async() => {
+        const api = await supertest(app)
+        .put("/activities/1")
+        .send({name:"name", image:"image.example"})
+        .set('Authorization', `Bearer ${admin}`)
+        .expect(400)
+    })
+})
+
+describe('PUT verified body', () =>{
+    test('There is no image', async() => {
+        const api = await supertest(app)
+        .put("/activities/1")
+        .send({content:"content", name:"name"})
+        .set('Authorization', `Bearer ${admin}`)
+        .expect(400)
+    })
+})
+
+describe('PUT verified body', () =>{
+    test('There is no Body', async() => {
+        const api = await supertest(app)
+        .put("/activities/1")
+        .send({})
+        .set('Authorization', `Bearer ${admin}`)
+        .expect(400)
+    })
+})
+
+describe('PUT verified body', () =>{
+    test('If there is data', async() => {
+        const api = await supertest(app)
+        .put("/activities/1")
+        .send({content:"content", name:"name", image:"image.example"})
+        .set('Authorization', `Bearer ${admin}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
+})
+
+describe('PUT verified Id', () =>{
+    test('ID not found', async() => {
+        const api = await supertest(app)
+        .put("/activities")
+        .set('Authorization', `Bearer ${admin}`)
+        .expect(404)
+    })
+})
