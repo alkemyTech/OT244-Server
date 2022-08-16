8edbdf740eb0d3849c5c69aa7765ad5fcdc5b564
mar-romero <romero-ar@hotmail.com>
2022-08-16 16:04:30 -0300

    Fix Arreglados

--- controllers/news.js
+++ controllers/news.js
@@ -79,21 +79,21 @@ async function getNew(request, response, next) {
 }
 
 const getAllNews = async (req, res) => {
-  const { page = 0 } = req.query;
-  const size = process.env.PAGE_SIZE;
-
+  const { page=0, limit=10 } = req.query;
+  const offset = parseInt(page * limit);
+  const endpoint = "news";
   try {
     const getData = await News.findAndCountAll({
-      limit: parseInt(size),
-      offset: parseInt(page * size),
+      limit: parseInt(limit),
+      offset
     });
 
     const pages = Math.floor(getData.count / size);
 
     res.status(200).json({
       getData,
-      next: nextPage(parseInt(page), pages),
-      prev: prevPage(parseInt(page), pages),
+      next: nextPage(endpoint,parseInt(page), pages),
+      prev: prevPage(endpoint,parseInt(page), pages),
     })
   } catch (error) {
     next(error)
--- helpers/paginationTools.js
+++ helpers/paginationTools.js
@@ -1,12 +1,12 @@
-const prevPage = (currentPage,totalPages) => {
+const prevPage = (location,currentPage,totalPages) => {
     if(currentPage > 0 && currentPage < totalPages){
-        return `${process.env.BASE_URL}categories?page=${currentPage-1}`
+        return `${process.env.BASE_URL+location}categories?page=${currentPage-1}`
     }
 }
 
-const nextPage = (currentPage,totalPages) => {
+const nextPage = (location,currentPage,totalPages) => {
     if(currentPage < totalPages){
-        return `${process.env.BASE_URL}categories?page=${currentPage+1}`
+        return `${process.env.BASE_URL+location}categories?page=${currentPage+1}`
     }
 }
 
