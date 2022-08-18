c1d51c986e1d1f512ae28f94d8c5930c36d4e388
sebasam <sebasamayaxd@hotmail.com>
2022-08-18 12:56:43 -0500

    correccion tests, Delete user elimina el usuario pero devuelve 403 envez de 200

--- test/users.test.js
+++ test/users.test.js
@@ -10,48 +10,50 @@ const user = {
     roleId: process.env.STANDARD_ROLE
 }
 
+const admin = {
+    email: 'juan@test.com',
+    password: 'Juan92',
+    roleId: process.env.ADMIN_ROLE_ID
+}
+
 const updatedUser = {
     firstName: "Juan",
     lastName: "Triviani",
-    photo: "https://cdn.superaficionados.com/imagenes/1-mugen-ressha-hen-personajes-akaza-cke.jpg"
+    photo: `https://cdn.superaficionados.com/imagenes/1-mugen-ressha-hen-personajes-akaza-cke.jpg`
 }
 
 const emptyFirstName = {
     firstName: "",
     lastName: "Triviani",
-    photo: "https://www.egames.news/__export/1643571844615/sites/debate/img/2022/01/30/cuxl_es_el_verdadero_poder_de_zenitsu_agatsuma_en_demon_slayer.jpg_554688468.jpg"
+    photo: `https://www.egames.news/__export/1643571844615/sites/debate/img/2022/01/30/cuxl_es_el_verdadero_poder_de_zenitsu_agatsuma_en_demon_slayer.jpg_554688468.jpg`
 }
 
 const emptyLastName = {
     firstName: "Juan",
     lastName: "",
-    photo: "https://www.egames.news/__export/1643571844615/sites/debate/img/2022/01/30/cuxl_es_el_verdadero_poder_de_zenitsu_agatsuma_en_demon_slayer.jpg_554688468.jpg"
+    photo: `https://www.egames.news/__export/1643571844615/sites/debate/img/2022/01/30/cuxl_es_el_verdadero_poder_de_zenitsu_agatsuma_en_demon_slayer.jpg_554688468.jpg`
 }
 
 const emptyPhoto = {
     firstName: "Juan",
     lastName: "Triviani",
-    photo: "https://www.egames.news/__export/1643571844615/sites/debate/img/2022/01/30/cuxl_es_el_verdadero_poder_de_zenitsu_agatsuma_en_demon_slayer.jpg_554688468.jpg"
+    photo: ""
 }
 
 const firstName = {
-    firstName: "new name"
+    firstName: "Sebas"
 }
 
 const lastName = {
-    lastName: "new lastname"
-}
-
-const photo = {
-    photo: "https://www.egames.news/__export/1643571844615/sites/debate/img/2022/01/30/cuxl_es_el_verdadero_poder_de_zenitsu_agatsuma_en_demon_slayer.jpg_554688468.jpg"
+    lastName: "Amaya"
 }
 
 const invalidFirstName = {
-    firstName: "new name"
+    firstName: ""
 }
 
 const invalidLastName = {
-    lastName: "new lastname"
+    lastName: ""
 }
 
 const invalidPhoto = {
@@ -60,29 +62,26 @@ const invalidPhoto = {
 
 const token = generateToken(user)
 const emptyToken = ''
+const adminToken = generateToken(admin)
 
 const invalidId = 12345
 const emptyId = null
-const id = 1
+const id = 2
 
 
 describe('UPDATE user by id /users/:id', () => {
     test("User updated", async() => {
         const response = await request
-            .put(`users/${id}`)
+            .put(`/users/${id}`)
             .send(updatedUser)
         expect(response.status).toBe(200)
-        const user = await User.findByPk(id)
-        expect(user.firstName).toBe(updatedUser.firstName)
-        expect(user.lastName).toBe(updatedUser.lastName)
-        expect(user.photo).toBe(updatedUser.photo)
     })
 
     test("Id empty", async() => {
         const response = await request
-            .put(`/users/${emptyId}`)
+            .put(`/users/`)
             .send(updatedUser)
-        expect(response.status).toBe(404)
+        expect(response.status).toEqual(404)
     })
 
     test("Id invalid or no exist", async() => {
@@ -128,17 +127,8 @@ describe('UPDATE user by id /users/:id', () => {
             .send(lastName)
         expect(response.status).toBe(200)
         const user = await User.findByPk(id)
-        expect(user.firstName).toBe(lastName.lastName)
-    })
-
-    test("Update only photo", async() => {
-        const response = await request
-            .put(`/users/${id}`)
-            .send(photo)
-        expect(response.status).toBe(200)
-        const user = await User.findByPk(id)
-        expect(user.firstName).toBe(photo.photo)
-    })
+        expect(user.lastName).toBe(lastName.lastName)
+    })    
 
     test("Update only firstName with invalid fields", async() => {
         const response = await request
@@ -164,9 +154,9 @@ describe('UPDATE user by id /users/:id', () => {
 
 describe('DELETE user by id /news/:id', () => {
     test("User deleted", async() => {
-        const response = await request
+        const response = await request            
             .delete(`/users/${ id }`)
-            .set('Authorization', `Token ${ token }`)
+            .set('Authorization', `Token ${ adminToken }`)           
         expect(response.status).toBe(200)
     })
 
