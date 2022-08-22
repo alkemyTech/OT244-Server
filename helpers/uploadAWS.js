6fa94b542e993bd04d1f0dfc387873ed9555e704
bluemountains88 <m.dariouk@gmail.com>
2022-08-22 19:13:45 -0300

    extension modified

--- helpers/uploadAWS.js
+++ helpers/uploadAWS.js
@@ -2,9 +2,10 @@ const s3 = require('../config/aws.js');
 const short = require('short-uuid');
 
 const uploadFile = async (file) => {
+    const extension = file.name.split('.').pop();
     const params = {
         Bucket: process.env.AWS_BUCKET,
-        Key: short.generate() + '-' + file.name,
+        Key: short.generate() + '.' + extension;
         Body: file.data,
         ACL: 'public-read'
     };
