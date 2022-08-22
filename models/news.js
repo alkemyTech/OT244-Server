408bbde283a03747f04567c98be6d40fbff58a5d
martin <martinc1998a@gmail.com>
2022-08-22 16:21:15 -0300

    adding news modification

--- models/news.js
+++ models/news.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      News.belongsTo(models.Categories)
+      /* News.belongsTo(models.Categories) */
     }
   };
   News.init({
