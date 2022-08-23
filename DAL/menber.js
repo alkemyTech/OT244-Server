d9dc3844d2af2a3a6faf946239fc9d1efbef3c2f
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-08-23 17:02:23 -0300

    asda

--- DAL/menber.js
+++ DAL/menber.js
@@ -4,9 +4,16 @@ const { Member } = require("../models");
 const getMembers = async (currentPage, numberMembers) => {
   const members = await Member.findAndCountAll({
     limit: parseInt(numberMembers),
-    offset:parseInt(currentPage * numberMembers),
-    attributes: ["name", "image", "description"]
-  })
+    offset: parseInt(currentPage * numberMembers),
+    attributes: [
+      "name",
+      "image",
+      "description",
+      "facebookUrl",
+      "instagramUrl",
+      "linkedinUrl",
+    ],
+  });
   return members
 }
 
